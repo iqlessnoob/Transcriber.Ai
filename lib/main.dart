8cd9f107bf3747c54f17a9cf690d619685cf998d
iqlessnoob <iqlessnoob@gmail.com>
2026-06-29 23:57:05 +0530

    Add import for sherpa_onnx package

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter_new/return_code.dart';
+
 import 'package:sherpa_onnx/sherpa_onnx.dart' as sherpa;
 
 void main() async {
